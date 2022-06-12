2c07e6fac47e9f7e1e4d8b426f1049ee6397655b
Philip Kvam <ph_kvam@hotmail.com>
2022-06-12 22:17:44 +0200

    validation on all inputs

---
+++ src/schemas/index.js
@@ -0,0 +1,37 @@
+import * as yup from "yup";
+
+export const validation = yup.object().shape({
+  registreringsNummer: yup
+    .string()
+    .required("Må fylles ut")
+    .matches(/^[a-z]{2}[\d]{5}$/, "Må inneholde 2 bokstaver + 5 tall"),
+
+  bonus: yup.string().ensure().required("Velg 1 bonus"),
+
+  // nummber som starter på 0 blir ikke registrert
+  fødselsNummer: yup
+    .number()
+    .required("Må være kun tall")
+    .test(
+      "len",
+      "Må være 11 siffer",
+      (val) => val && val.toString().length === 11
+    ),
+
+  fornavn: yup
+    .string()
+    .required("Må fylles ut")
+    .min(2, "Minimum 2 bokstaver")
+    .max(30),
+
+  etternavn: yup
+    .string()
+    .required("Må fylles ut")
+    .min(2, "Minimum 2 bokstaver")
+    .max(30),
+
+  epost: yup
+    .string()
+    .email("Skriv en gyldig e-postadresse")
+    .required("Må fylles ut"),
+});
